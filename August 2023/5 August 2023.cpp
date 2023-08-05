d96ffd3c34319ccf6b588bb8ea97a95dc31fdd1f
Harshita Kaushik <harshitakaushik.hk@gmail.com>
2023-08-05 09:40:28 +0530

    95. Unique Binary Search Tree II

---
+++ August 2023/5 August 2023.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<TreeNode*> buildTrees(int l, int r)
+    {
+      if(l > r) return {nullptr};
+      vector<TreeNode*> ans;
+
+      for(int i=l; i<=r; i++)
+      {
+        vector<TreeNode*> left = buildTrees(l, i-1);
+        vector<TreeNode*> right = buildTrees(i+1, r);
+
+        {
+          for(int k=0; k<right.size(); k++)
+          {
+            ans.push_back(new TreeNode(i, left[j], right[k]));
+          }
+        }
+      }
+      return ans;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return buildTrees(1,n);
+    }
+};
